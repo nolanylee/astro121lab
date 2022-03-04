b4af09bbda8ba22472e02d5bafec18d198aa2851
nolanylee <nolanylee@berkeley.edu>
2022-03-04 21:02:44 +0000

    wrote observing script

--- interf.py
+++ interf.py
@@ -19,17 +19,19 @@ import scipy
 # ifm.point(alt,az) points telescopes at specified alt,az
 
 def moon_point():
-   time = time.now() 
-   moon_ra, moon_dec = ugradio.coord.moonpos(time,lat,lon,alt)
-   precess_ra, precess_dec = ugradio.coord.precess(moon_ra,
-                                                   moon_dec,
-                                                   time,
-                                                   equinox)     
-   moon_alt, moon_az = ugradio.coord.get_altaz(precess_ra,
-                                               precess_dec,
-                                               time,
-                                               equinox)
-   return ifm.point(moon_alt,moon_az)
+    time = ugradio.timing.julian_date()
+    lat, lon, alt = ugradio.nch
+    equinox = 'J2000'
+    moon_ra, moon_dec = ugradio.coord.moonpos(time,lat,lon,alt)
+    precess_ra, precess_dec = ugradio.coord.precess(moon_ra,
+                                                    moon_dec,
+                                                    time,
+                                                    equinox)     
+    moon_alt, moon_az = ugradio.coord.get_altaz(precess_ra,
+                                                precess_dec,
+                                                time,
+                                                equinox)
+    return moon_alt, moon_az
 
 #to do:
 #make sure time.now() is in units of jd
@@ -42,3 +44,26 @@ def moon_point():
 # determine how frequently we should save the h2h data
 # stop collecting data when finished (when?)
 # stow telescopes
+
+def observe():
+    ifm = ugradio.interf.Interferometer()
+    hpm = ugradio.hp_multi.HP_Multimeter()
+    hpm.start_recording(10)
+    all_voltages = []
+    all_times = []
+    for i in range(5):
+        moon_alt, moon_az = moon_point()
+        ifm.point(moon_alt, moon_az)
+        east_moving = True  # telescope is moving
+        west_moving = True
+        while east_moving or west_moving:
+            west_moving = (ifm.get_pointing()['ant_w'] != moon_alt, moon_az)
+            east_moving = (ifm.get_pointing()['ant_e'] != moon_alt, moon_az)
+            time.sleep(1.)
+        time.sleep(12.)
+        voltages, times = hpm.get_recording_data()
+        all_voltages.append(voltages)
+        all_times.append(times)
+    hpm.end_recording()
+    ifm.stow()
+    np.savez('observing_data', all_voltages=all_voltages, all_times=all_times)
