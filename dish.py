403898106c087837fbfe4cdbbe2a9e7f7376cd39
Cole Forrester <crforrester@berkeley.edu>
2022-04-15 15:25:40 -0700

    fixed bubble_coords and initialized obs function

--- dish.py
+++ dish.py
@@ -2,38 +2,52 @@ import ugradio
 from ugradio.leo import *
 import numpy as np
 import pandas as pd
-import csv
+import astropy
 from astropy.time import Time
 from astropy.coordinates import Angle, SkyCoord, EarthLocation, AltAz
 
 def bubble_coords():
-    coords = {}
-    b = np.arange(-70, -8, 2)
-    for i in b:
-        l = np.arange(160, 220.001, 2 / np.cos(i*np.pi/180))
-        coords[i] = l
-    with open('bubble_coords.csv', 'w') as f:
-        for key in coords.keys():
-            f.write("%s,%s\n"%(key, coords[key]))
+    coords = []
+    bs = np.arange(-70, -8, 2)
+    for b in bs:
+        ls = np.arange(160, 220.1, 2/np.cos(b*np.pi/180))
+        for l in ls:
+            coords.append((l,b))
+    return coords
 
-def gal2altaz(l,b): 
+def gal2altaz(l, b, jd): 
     gal_coord = SkyCoord(l=l, b=b, frame='galactic', unit='deg')
-    time = Time.now()
+    #time = Time.now()
     #campbell = EarthLocation(lat=37.873199 * u.deg, lon=-122.27573 * u.deg, height=20 * u.meter)
     leuschner = EarthLocation(lat = lat, lon = lon, height = alt)
-    altaz_frame = AltAz(obstime = time, location = leuschner)
+    altaz_frame = AltAz(obstime = jd, location = leuschner)
     altaz_coord = gal_coord.transform_to(altaz_frame)
     return altaz_coord.alt, altaz_coord.az
 
-def test_obs(l,b):
-    alt, az = gal2altaz(l,b)
-    ugradio.LeuschTelescop.point(alt, az)
-    spec = gradio.leusch.Spectrometer()
-    spec.Spectrometer.read_spec('test.fits,', 1000, (l,b), 'ga')
-    sd = ugradio.agilent.SynthDirect()
-    sd.SynthClient.set_frequency(1e6)
-    spec.Spectrometer.read_spec('test_LO.fits', 1000, (l,b), 'ga')
-    LeuschTelescope.stow()
+def bubble_obs(l, b, jd, nspec):
+
+    telescope = ugradio.leusch.LeuschTelescope()
+    spectrometer = ugradio.leusch.Spectrometer()
+    noise = ugradio.leusch.LeuschNoise()
+
+    alt, az = gal2altaz(l,b,jd)
+
+    print(f'Pointing to coordinates {alt, az...}')
+    try:
+        telescope.point(alt, az)
+        print('Activating noise diode...')
+        noise.on()
+        print('Gathering spectra (diode on)')
+        spectrometer.read_spec(jd+'_nd1.fits', nspec, (l,b), 'ga')
+        print('Deactivating noise diode')
+        noise.off()
+        print('Gathering Spectra (diode off)')
+        spectrometer.read_spec(jd+'_nd0.fits', nspec, (l,b), 'ga')
+    except AssertionError:
+        print('YOU FOOL: You pointed the telescope out of bounds')
+        break
+
+    telescope.stow()
 
 
-
+    
