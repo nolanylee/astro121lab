8381e3215f74907c314edf012c14daeec1a9b067
Cole Forrester <crforrester@berkeley.edu>
2022-04-16 13:38:02 -0700

    made some tweaks

--- dish.py
+++ dish.py
@@ -15,38 +15,44 @@ def bubble_coords():
             coords.append((l,b))
     return coords
 
-def gal2altaz(l, b, jd): 
+def gal2altaz(l, b): 
     gal_coord = SkyCoord(l=l, b=b, frame='galactic', unit='deg')
     #time = Time.now()
     #campbell = EarthLocation(lat=37.873199 * u.deg, lon=-122.27573 * u.deg, height=20 * u.meter)
     leuschner = EarthLocation(lat = lat, lon = lon, height = alt)
-    altaz_frame = AltAz(obstime = jd, location = leuschner)
+    altaz_frame = AltAz(obstime = Time.now(), location = leuschner)
     altaz_coord = gal_coord.transform_to(altaz_frame)
     return altaz_coord.alt, altaz_coord.az
 
-def bubble_obs(l, b, jd, nspec):
+def bubble_obs(l, b, nspec):
 
     telescope = ugradio.leusch.LeuschTelescope()
     spectrometer = ugradio.leusch.Spectrometer()
     noise = ugradio.leusch.LeuschNoise()
 
-    alt, az = gal2altaz(l,b,jd)
+    coords = bubble_coords()
 
-    print(f'Pointing to coordinates {alt, az...}')
-    try:
-        telescope.point(alt, az)
-        print('Activating noise diode...')
-        noise.on()
-        print('Gathering spectra (diode on)')
-        spectrometer.read_spec(jd+'_nd1.fits', nspec, (l,b), 'ga')
-        print('Deactivating noise diode')
-        noise.off()
-        print('Gathering Spectra (diode off)')
-        spectrometer.read_spec(jd+'_nd0.fits', nspec, (l,b), 'ga')
-    except AssertionError:
-        print('YOU FOOL: You pointed the telescope out of bounds')
-        break
+    for i in len(coords):
 
+        l, b = coords[i]
+        alt, az = gal2altaz(l,b)
+
+        print(f'Pointing to coordinates {alt, az...}')
+        try:
+            telescope.point(alt, az)
+            print('Activating noise diode...')
+            noise.on()
+            print('Gathering spectra (diode on)')
+            spectrometer.read_spec(f'{jd}_nd1.fits', nspec, (l,b), 'ga')
+            print('Deactivating noise diode')
+            noise.off()
+            print('Gathering Spectra (diode off)')
+            spectrometer.read_spec(f'{jd}_nd0.fits', nspec, (l,b), 'ga')
+        except AssertionError:
+            print('YOU FOOL: You failed to point the telescope')
+            break
+    
+    print('Stowing telescope')
     telescope.stow()
 
 
